74b76c58dbfc483e20ad732491634679c279a25d
Lizbeth_Santamaria <lizbeth.santamaria@epn.edu.ec>
2023-01-02 17:36:54 -0500

    02-typeScript --> 01-variables
    Introduccion a typeScript

---
+++ 02-typescript/01-variables.ts
@@ -0,0 +1,14 @@
+// 01-variables.ts
+// npm install -g typescript
+// tsc
+// 01-variables.ts
+var nombre = 'Adrian'; // primitivo
+var nombre2 = 'Adrian2'; // Clase String
+// tsc 01-variables.ts --target es3
+// tsc 01-variables.ts --target es6nombre = 1;
+// nombre = 1;
+var edad = 32;
+var casado = false;
+var fecha = new Date();
+var sueldo;
+sueldo = 12.4;
