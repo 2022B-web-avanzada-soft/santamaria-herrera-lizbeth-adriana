34c5407feb4c9238ec8fad699f92466c44eb8211
Lizbeth_Santamaria <lizbeth.santamaria@epn.edu.ec>
2023-03-13 16:58:42 -0500

    MetodosHTTP_Crear_Actualizar_Eliminar_Buscar
    2023-02-23

--- 04-nestjs-websockets/websockets/src/usuario/usuario.controller.ts
+++ 04-nestjs-websockets/websockets/src/usuario/usuario.controller.ts
@@ -1,6 +1,90 @@
-import {Controller} from "@nestjs/common";
+import {BadRequestException, Body, Controller, Delete, Get, HttpCode, Param, Post, Put, Query} from "@nestjs/common";
+import {UsuarioService} from "./usuario.service";
+import {UsuarioUpdateDto} from "./dto/usuario-update.dto";
+import {validate} from "class-validator";
+import {UsuarioCreateDto} from "./dto/usuario-create.dto";
+
 
 @Controller('usuario')
+// http://localhost:3000/usuario/
+// @Controller('usuario/asd/qwe')
+// http://localhost:3000/usuario/asd/qwe
 export class UsuarioController{
+    constructor(
+        private readonly usuarioService: UsuarioService
+    ) {
+    }
+    @Get("/:id") // GET /usuario/1
+    @HttpCode(200)
+    findOneById(
+        // "/:id/notas/:idNota"
+        @Param() params // {id:1, idNota:12}
+    ) {
+        return this.usuarioService.findOneById(+params.id); // +"1" = 1
+    }
 
+    @Delete("/:id") // DELETE /usuario/:id
+    @HttpCode(200)
+    delete(
+        // "/:id/notas/:idNota"
+        @Param() params // {id:1, idNota:12}
+    ) {
+        return this.usuarioService.delete(+params.id);
+    }
+
+    @Put("/:id") // PUT /usuario/:id
+    @HttpCode(200)
+    async update(
+        @Param() params, // {id:1}
+        @Body() bodyParams // {nombres:''....}
+    ) {
+        const nuevoRegistro = new UsuarioUpdateDto();
+        nuevoRegistro.nombres = bodyParams.nombres;
+        nuevoRegistro.apellidos = bodyParams.apellidos;
+        nuevoRegistro.rol = bodyParams.rol;
+        const arregloErrores = await validate(
+            nuevoRegistro
+        ); // validamos
+        if (arregloErrores.length > 0) {
+            console.error({arregloErrores});
+            throw new BadRequestException({
+                mensaje: 'Envio mal datos'
+            });
+        }
+        return this.usuarioService.update(
+            bodyParams,
+            +params.id
+        );
+    }
+
+    @Post("/") // POST /usuario
+    @HttpCode(201)
+    async create(
+        @Body() bodyParams // {nombres:''....}
+    ) {
+        const nuevoRegistro = new UsuarioCreateDto();
+        nuevoRegistro.nombres = bodyParams.nombres;
+        nuevoRegistro.apellidos = bodyParams.apellidos;
+        nuevoRegistro.rol = bodyParams.rol;
+        const arregloErrores = await validate(
+            nuevoRegistro
+        ); // validamos
+        if (arregloErrores.length > 0) {
+            console.error({arregloErrores});
+            throw new BadRequestException({
+                mensaje: 'Envio mal datos'
+            });
+        }
+        return this.usuarioService.create(nuevoRegistro);
+    }
+
+    // 1 Informativo
+    // 2 OK
+    // 3 Redireccion
+    // 4 Error cliente
+    // 5 Error servidor
+    // parametros:
+    // 1 QueryParams ?id=1&consulta=Adrian
+    // 2 BodyParams (viajan en el formulario)
+    // 3 Parametros de ruta /usuario/:id/notas
 }
--- 04-nestjs-websockets/websockets/src/usuario/usuario.service.ts
+++ 04-nestjs-websockets/websockets/src/usuario/usuario.service.ts
@@ -1,14 +1,39 @@
 import {Injectable} from "@nestjs/common";
-import {InjectRepository} from "@nestjs/typeorm";
+import {InjectDataSource} from "@nestjs/typeorm";
 import {UsuarioEntity} from "./usuario.entity";
-import {Repository} from "typeorm";
+import {DataSource, FindManyOptions} from "typeorm";
+import {UsuarioCreateDto} from "./dto/usuario-create.dto";
+import {UsuarioUpdateDto} from "./dto/usuario-update.dto";
 
 @Injectable()
 export class UsuarioService{
     constructor(
-        @InjectRepository(UsuarioEntity, 'default')
-        public usuarioRepository: Repository<UsuarioEntity>
-    ) {
+        @InjectDataSource()
+        public datasource: DataSource
+    ) {}
+    public usuarioRepository = this.datasource.getRepository(UsuarioEntity)
 
+    find(opciones: FindManyOptions<UsuarioEntity>) {
+        return this.usuarioRepository.find(opciones)
+    }
+    findOneById(id: number) {
+        return this.usuarioRepository.findOne({
+            // select:{ },
+            where: {
+                id: id
+            },
+        })
+    }
+
+    create(datosCrear: UsuarioCreateDto) {
+        return this.usuarioRepository.save(datosCrear);
+    }
+    update(datosActualizar: UsuarioUpdateDto, id: number) {
+        return this.usuarioRepository.save(
+            {...datosActualizar, id}
+        );
+    }
+    delete(id: number) {
+        return this.usuarioRepository.delete(id);
     }
 }
